731a8b97aa4c91329e098aab3bd2c9afc5442ca7
Igor Walkowiak <igorwalkowiak@protonmail.com>
2021-01-11 20:53:20 +0100

    Frontend stub connection added

--- app.py
+++ app.py
@@ -10,6 +10,14 @@ init_db()
 def home():
     return render_template('index.html')
 
+@app.route('/cart')
+def cart():
+    return render_template('cart.html')
+
+@app.route('/catalog')
+def catalog():
+    return render_template('browse_categories.html')
+
 
 if __name__ == '__main__':
     app.run()
