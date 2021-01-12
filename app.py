efc6dfbc98b5ccb2224b91f1a787d99392304334
Pawel <mrprzyprawa@gmail.com>
2021-01-12 15:17:32 +0100

    Refactor navbar styles

--- app.py
+++ app.py
@@ -10,22 +10,22 @@ init_db()
 
 @app.route('/')
 def home():
-    return render_template('template.html', title="Index", current_time=datetime.datetime.now())
+    return render_template('template.html', title="Strona główna")
 
 @app.route('/cart')
 def cart():
     return render_template('cart.html',
-        my_list=[], title="Koszyk", current_time=datetime.datetime.now())
+        my_list=[], title="Koszyk")
 
 @app.route('/catalog')
 def catalog():
     categories = Category.query.all()
-    return render_template('browse_categories.html', categories=categories)
+    return render_template('browse_categories.html', categories=categories, title='Kategorie')
 
 @app.route('/category/<category_id>')
 def category(category_id):
     prod_in_category = Product.query.filter(Product.category == category_id).all()
-    return render_template('browse_category.html', products=prod_in_category)
+    return render_template('browse_category.html', products=prod_in_category, title=f'Kategoria {category_id}')
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080)
