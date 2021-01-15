9f1adef50248e191c434b5286ade0db24d37b60d
Igor Walkowiak <igorwalkowiak@protonmail.com>
2021-01-15 19:39:03 +0100

    dsa

--- unit_tests.py
+++ unit_tests.py
@@ -7,7 +7,7 @@ class Test_Product(unittest.TestCase):
         prod = product.Product('test', 1, 5)
         self.assertEqual(prod.name, 'test')
         self.assertEqual(prod.amount, 1)
-        self.assertEqual(prod.price, 2)
+        self.assertEqual(prod.price, 5)
 
     def whenInputaOkThenNoException(self):
         prod = product.Product('test', 1, 2)
