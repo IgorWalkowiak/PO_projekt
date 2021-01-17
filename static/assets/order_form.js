737a521aae1d31bc7e8160a6cd089d831aaca41f
Pawel <mrprzyprawa@gmail.com>
2021-01-17 22:52:42 +0100

    Add form validation

--- static/assets/order_form.js
+++ static/assets/order_form.js
@@ -1,11 +1,18 @@
 const deliveryRadios = document.querySelectorAll(".delivery-radio");
 const addressForm = document.querySelectorAll("#address")[0];
+const addressInput = document.querySelectorAll(".address-input");
 
 function showIfNotPickupInPerson() {
   if (this.checked && this.defaultValue !== "pickup-in-person") {
     addressForm.classList.add("address-form-active");
+    for (let i = 0; i < addressInput.length - 1; i++) {
+      addressInput[i].required = true;
+    }
   } else {
     addressForm.classList.remove("address-form-active");
+    for (let i = 0; i < addressInput.length - 1; i++) {
+      addressInput[i].required = false;
+    }
   }
 }
 
